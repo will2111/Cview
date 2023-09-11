ed6f71a52fec2299839c70a822e017a6472bb571
will2111 <95877699+will2111@users.noreply.github.com>
2023-09-11 15:00:37 +0200

    Update Env42

--- cview_inst.sh
+++ cview_inst.sh
@@ -1,15 +1,10 @@
 #!bin/bash/
 
-test="$(cat ~/.bash_aliases | grep cview)"
+test="$(cat ~/.zshrc | grep cview)"
 
 if [ -z "$test" ] 
 then
-	if [ ! -f "~/.bash_aliases" ]
-	then
-		touch ~/.bash_aliases
-	fi
-	
-	cat ./cview >> ~/.bash_aliases
+	cat ./cview >> ~/.zshrc
 	echo $'\e[1:37m'
 	printf "\n\n%s\n" 'La commande cview a bien été installé'
 	printf "\n%s\n\n" 'cat ~/.bash_aliases | grep cview'
